0119348f4b9cb8208c1c92d4f4eaebeef693fc9c
sharmilee <sharmilee@LAPTOP-4TQ44UI1>
2020-04-03 22:48:15 +0530

    Assignment1 complete commit

--- Java_New/src/javaNew.java
+++ Java_New/src/javaNew.java
@@ -2,14 +2,17 @@
 //comment the above line and uncomment below line to use Chrome
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 public class javaNew {
 	public static void main(String[] j) throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\Selenium Drivers\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
-	        driver.get("https://demoqa.com/selectable");
+		//Click on each item and print their names
+	   driver.get("https://demoqa.com/selectable");
        Thread.sleep(2000);
        
        driver.findElement(By.xpath("//*[@id='selectable']/li[1]")).click();
@@ -31,7 +34,40 @@ public class javaNew {
        
        System.out.println(item1 +" "+item2 +" "+ item3 +" "+ item4 +" "+ item5 +" "+ item6 +" "+ item7);
       
-        //close Fire fox
+      //Fill the contact form an submit
+       driver.get("https://demoqa.com/html-contact-form");
+       Thread.sleep(10000);
+       
+       driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/form/input[1]")).sendKeys("web");
+       Thread.sleep(10000);
+       driver.findElement(By.id("lname")).sendKeys("Automation");
+       Thread.sleep(10000);
+       driver.findElement(By.name("country")).sendKeys("India");
+       Thread.sleep(10000);
+       driver.findElement(By.id("subject")).sendKeys("NA");
+       Thread.sleep(10000);
+       driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/form/input[4]")).click();
+       Thread.sleep(10000);
+       
+      
+		// Darg and drop
+		 driver.get("https://demoqa.com/droppable"); Thread.sleep(10000);		 
+		 WebElement From = driver.findElement(By.xpath("//*[@id='draggable']"));
+		 WebElement To = driver.findElement(By.xpath("//*[@id='droppable']"));
+		 
+		  Actions act = new Actions(driver);
+		  act.dragAndDrop(From,To).build().perform();
+		  driver.findElement(By.xpath("//*[@id='droppable']/p"));
+		 
+       // Choose DOB
+       driver.get("https://demoqa.com/datepicker");
+       Thread.sleep(10000);
+       driver.findElement(By.id("datepicker")).click();
+       Thread.sleep(10000);
+       driver.findElement(By.xpath("//*[@id=\"ui-datepicker-div\"]/table/tbody/tr[1]/td[7]/a")).click();
+       Thread.sleep(10000);
+       System.out.println("passed");
+        //close Chrome
         driver.close();
 	}
 
